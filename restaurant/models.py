d1c41df29e60ed8bb372613976a37ccb63906dcd
admin <aoclakhi@gmail.com>
2026-05-07 13:07:13 +0530

    Little Lemon Capstone Project

---
+++ restaurant/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.IntegerField()
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
---
+++ restaurant/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import MenuView, BookingView
+
+urlpatterns = [
+    path('menu/', MenuView.as_view()),
+    path('booking/', BookingView.as_view()),
+]
